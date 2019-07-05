f0ab675802fdf864253733b8d916bb61df2634cb
362515241013 <52291403+362515241013@users.noreply.github.com>
2019-07-05 14:03:49 +0700

    Add files via upload

---
+++ lukkrok#1.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[7]:
+
+
+print("................................")
+print("WoratahtSh Shop")
+print("................................")
+print("CIVIT X 1    : 15    THB")
+print("WATER X 3  : 15    THB")
+print("JIMMY X 3   : 15    THB")
+print( "Total          : 45     THB")
+
+
+# In[15]:
+
+
+print(type(10))
+print(type("hello"))
+print(type(True))
+print(type(10.3))
+print(type(1j))
+print(10+3.5)
+
+
+# In[ ]:
+
+
+
+
