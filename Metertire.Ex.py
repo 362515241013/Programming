b6513ed4b1e67433ca7b213dbe5cef0314594108
362515241013 <52291403+362515241013@users.noreply.github.com>
2019-07-05 16:04:08 +0700

    Add files via upload

---
+++ Metertire.Ex.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[20]:
+
+
+print("woratahtSh shop" )
+print(".........................")
+print("milk"      , 1 , 10, "THB",(1*10) ,"THB" )
+print("banana" , 1 ,  20, "THB",(1*20),"THB")
+print("water"    , 1 , 10, "THB",(1*10),"THB")
+print("..........................")
+print("Total"        ,(10+20+10),   "THB")
+
+
+# In[ ]:
+
+
+
+
---
+++ String.Ex1.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[7]:
+
+
+print("hello")
+print(type("hello"))
+print("hello"+"world")
+print("hello"*5)
+
+
+# In[ ]:
+
+
+
+
