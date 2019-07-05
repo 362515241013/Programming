4f8a8470f46399fe455c26dc128a8136e6b31f00
362515241013 <52291403+362515241013@users.noreply.github.com>
2019-07-05 16:04:25 +0700

    Add files via upload

---
+++ bool.Ex1.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+print(True)
+print(False)
+
+
+# In[7]:
+
+
+print(type(True))
+print(False and True)
+
+
+# In[11]:
+
+
+print(True)
+print(False or True)
+
+
+# In[ ]:
+
+
+
+
---
+++ float.ex1.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[8]:
+
+
+print(type(3.14))
+print(10+3.14)
+print(10+3.0)
+print(10-2.0)
+print(10/2.0)
+print(10*2.0)
+
+
+# In[ ]:
+
+
+
+
