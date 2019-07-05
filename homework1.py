5155ef541785b7304b1e4f9307664ecb996f75f1
362515241013 <52291403+362515241013@users.noreply.github.com>
2019-07-05 16:03:34 +0700

    Add files via upload

---
+++ homework1.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[15]:
+
+
+print("CP ALL, 7-Eleven ลาดกระบัง 11/4 (9725)")
+print("TAX#0107542000011(VAT Included)")
+print("Vat 07507 POS : D9408C0003400188")
+print("ใบเสร็จรับเงิน/ใบกำกับภาษี")
+print("1  *Cash card  100          100.00")
+print("Total        (1+ 100.00)")
+print("Cash/Change   100.00       0.00")
+
+
+# In[ ]:
+
+
+
+
