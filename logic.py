5a4180e5f2b07dda435dc5754f53b79691f50367
362515241013 <52291403+362515241013@users.noreply.github.com>
2019-09-13 16:58:51 +0700

    Add files via upload

---
+++ logic.py
@@ -0,0 +1,13 @@
+a=True 
+b=False
+print("ANDLogic:") 
+print("aanda=",a and a) 
+print("aandb=",a and b) 
+print("bandb=",b and b) 
+print("\nORLogic:") 
+print("a or a="	,a or a) 
+print("a or b="	,a or b) 
+print("b or b="	,b or b)
+print("\nNOTLogic:") 
+print("a=",a,"\tnota=",nota) 
+print("b=",b,"\tnotb=",notb) 
